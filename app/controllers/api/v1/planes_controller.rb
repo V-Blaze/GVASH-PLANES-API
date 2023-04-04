fd0fb7fb601ecb69cae8b8eb94aac5c449c24779
V-Blaze <valentine6586@gmail.com>
2023-04-04 15:33:29 +0100

    Add plane create method

--- app/controllers/api/v1/planes_controller.rb
+++ app/controllers/api/v1/planes_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::PlanesController < ApplicationController
+  # before_action :set_user, only: %i[create]
+
   def index
     @planes = latest_planes(plane_index_params[:offset], plane_index_params[:limit])
     render json: PlaneSerializer.new(@planes).serializable_hash.to_json
+  end
+
+  def create
+    return render json: { error: "You don't have enough permission to create new plane"}, status: :unprocessable_entity unless admin?
+
+    @plane = Plane.new(plane_params)
+    @plane.user = @current_user
+
+    if @plane.save
+      render json: {plane: @plane, message: 'Plane created successfully' }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -12,5 +27,14 @@ class Api::V1::PlanesController < ApplicationController
 
   def plane_index_params
     params.permit(:offset, :limit)
+  end
+
+  def admin?
+    @current_user.id == 1
+  end
+
+  def plane_params
+    params.permit(:name, :plane_type, :description, :image, :price,
+                  :model, :year_of_manufacture, :life_span, :fees)
   end
 end
---
+++ db/migrate/20230404141831_create_planes_reservations.rb
@@ -0,0 +1,3 @@
+class CreatePlanesReservations < ActiveRecord::Migration[7.0]
+
+end
