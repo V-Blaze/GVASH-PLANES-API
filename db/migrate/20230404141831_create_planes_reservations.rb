b64b8d1a250bc857cdf30703b39d26b693ec9fac
V-Blaze <valentine6586@gmail.com>
2023-04-04 15:34:18 +0100

    Generate plane_reservation migration

--- db/migrate/20230404141831_create_planes_reservations.rb
+++ db/migrate/20230404141831_create_planes_reservations.rb
@@ -1,3 +1,13 @@
 class CreatePlanesReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :planes_reservations do |t|
+      t.date :date
+      t.time :duration
 
+      t.references :user, null: false, foreign_key: true
+      t.references :plane, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
 end
