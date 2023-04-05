b0848a4e1cb4cbc823160a80ab24a2d93f4966eb
ginabeki <ginabeki3@gmail.com>
2023-04-05 16:55:41 +0300

    Add integration test

---
+++ app/models/plane_reservation.rb
@@ -0,0 +1,17 @@
+class PlaneReservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :plane
+
+  validates :date, :user_id, :plane_id, :start_time, :end_time, presence: true
+
+  validate :no_conflicting_reservations
+  
+  def no_conflicting_reservations
+    if PlaneReservation.where(plane_id: plane_id)
+                        .where('start_time < ? AND end_time > ?', end_time, start_time)
+                        .where.not(id: id)
+                        .exists?
+      errors.add(:base, "This plane is already reserved at this time.")
+    end
+  end
+end
