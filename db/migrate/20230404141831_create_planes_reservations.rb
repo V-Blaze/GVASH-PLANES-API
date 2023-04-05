a3b374ee7097e1fd2af783f4bb6041bd49c85c38
ginabeki <ginabeki3@gmail.com>
2023-04-05 16:53:32 +0300

    Add start_time and end_time column

--- db/migrate/20230404141831_create_planes_reservations.rb
+++ db/migrate/20230404141831_create_planes_reservations.rb
@@ -1,8 +1,10 @@
 class CreatePlanesReservations < ActiveRecord::Migration[7.0]
   def change
-    create_table :planes_reservations do |t|
+    create_table :plane_reservations do |t|
       t.date :date
-      t.time :duration
+      t.integer :duration
+      t.time :start_time
+      t.time :end_time
 
       t.references :user, null: false, foreign_key: true
       t.references :plane, null: false, foreign_key: true
