d8e72fc19b9eddf047ef3478142dccfa80ddb930
ginabeki <ginabeki3@gmail.com>
2023-04-12 13:54:51 +0300

    Update date reset error

--- app/controllers/api/v1/planes_reservations_controller.rb
+++ app/controllers/api/v1/planes_reservations_controller.rb
@@ -12,11 +12,16 @@ class Api::V1::PlanesReservationsController < ApplicationController
     plane_reservation = @current_user.plane_reservations.build(plane_reservation_params)
 
     if plane_reservation.start_time.present? && plane_reservation.end_time.present?
-      duration = ((plane_reservation.end_time - plane_reservation.start_time) / 60).to_i
-      plane_reservation.duration = duration
+      duration_in_minutes = ((plane_reservation.end_time - plane_reservation.start_time) / 60).to_i
+      plane_reservation.duration = duration_in_minutes
 
       if plane_reservation.save
-        render json: { plane_reservation:, duration: }, status: :created
+        duration_in_hours = duration_in_minutes / 60
+        duration_in_minutes %= 60
+        duration_formatted = format('%<duration_in_hours>d:%<duration_in_minutes>02d',
+                                    duration_in_hours:, duration_in_minutes:)
+
+        render json: { plane_reservation:, duration: duration_formatted }, status: :created
       else
         render json: { errors: plane_reservation.errors.full_messages }, status: :unprocessable_entity
       end
