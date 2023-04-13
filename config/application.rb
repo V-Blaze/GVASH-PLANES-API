d006f3ceb63d9f51c6872b2434f1b9ac192af781
V-Blaze <valentine6586@gmail.com>
2023-04-13 12:30:29 +0100

    Update asset precompile to false

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module GVASHPlanesApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.enabled = false
 
     # Configuration for the application, engines, and railties goes here.
     #
