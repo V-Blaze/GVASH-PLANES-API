98b514b7e2c9c20383d7a16896b6b8dfedb41eca
V-Blaze <valentine6586@gmail.com>
2023-04-13 16:58:17 +0100

    Generate secret key for jwt

--- app/controllers/concerns/json_web_token.rb
+++ app/controllers/concerns/json_web_token.rb
@@ -2,7 +2,8 @@ require 'jwt'
 
 module JsonWebToken
   extend ActiveSupport::Concern
-  SECRET_KEY = Rails.application.secrets.secret_key_base
+  # SECRET_KEY = Rails.application.secrets.secret_key_base
+  SECRET_KEY = Rails.application.credentials.secret_key_base
   ENCODING = 'HS512'.freeze
   def jwt_encode(payload, exp = 7.days.from_now)
     payload[:exp] = exp.to_i
